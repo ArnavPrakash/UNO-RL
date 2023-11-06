af20196035da95c80319323494f3f1eb93609f66
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2023-11-07 00:11:04 +0530

    Final Commit for buildDeck.py

--- UNO_GAME/buildDeck.py
+++ UNO_GAME/buildDeck.py
@@ -12,16 +12,14 @@ def buildDeck():
     values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Draw Two", "Skip", "Reverse"]
     wilds = ["Wild", "Wild Draw Four"]
 
-    for wild in range(4):
-        deck.append(wilds[0])
-        deck.append(wilds[1])
-
     for color in colors:
         for value in values:
             cardVal = "{} {}".format(color, value)
             deck.append(cardVal)
-            if (value != 0):
+            if value != 0:
                 deck.append(cardVal)
-
+    for _ in range(4):
+        deck.append(wilds[0])
+        deck.append(wilds[1])
     return deck
 
