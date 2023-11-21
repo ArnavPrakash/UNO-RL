772286127bfdfe5840a9bc209b3756b4ce42410a
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-11-21 18:11:47 +0530

    startup scripts done

---
+++ config.py
@@ -0,0 +1,12 @@
+params = {
+    "iterations": 10000,
+    "algorithm": "monte-carlo", # ["q-learning", "monte-carlo"]
+    "logging": True,
+    "model": {
+        "epsilon": 0.4,
+        "step_size": 0.2,
+    }
+}
+
+player_name_1 = "Player1"
+player_name_2 = "Player2"
